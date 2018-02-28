378abb6019dc0beded279c810e86b3abad38d3b5
Ilkya Kutko <36884482+DyatelVudu@users.noreply.github.com>
2018-02-28 22:28:36 +0300

    done2

--- src/index.js
+++ src/index.js
@@ -2,26 +2,28 @@ module.exports = function solveSudoku(matrix) {
   for (let row = 0; row < 9; row++) {
     for (let col = 0; col < 9; col++) {
         if (matrix[row][col] === 0) {
-          let s = suggest(row, col);
-            for (var suggestion of s) {
+          let s = count(row, col, matrix);
+            for (let suggestion of s) {
                 matrix[row][col] = suggestion;
                 solveSudoku(matrix);
-            }  
+            }
         }
     }
 }
 return matrix;
 }
 
- function suggest(row,col) {
-    let result = [];
-    let zone = {
-        row: Math.floor(row/3)*3,
-        col: Math.floor(col/3)*3,
-    };
+function count(row,col,matrix) {
+  let result = [];
+  row = help(row)*3;
+  col = help(col)*3;
 
-    for (let i = 0; i < 9; i++) {
-        result.push([matrix[row][i], matrix[i][col], matrix[zone.row + i % 3][zone.col + Math.floor(i / 3)]])
-    }
-    return result;
+  for (let i = 0; i < 9; i++) {
+      result.push([matrix[row][i], matrix[i][col], matrix[row + i % 3][col + help(i)]])
+  }
+  return result;
+}
+
+function help(a){
+    return Math.floor(a/3);
 }
